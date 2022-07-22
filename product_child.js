73f01b818996d3f52ce84838188c75a77dd6c120
Thuynt2003 <101488871+Thuynt2003@users.noreply.github.com>
2022-07-22 09:17:05 +0700

    done

--- product_child.js
+++ product_child.js
@@ -1,46 +1,46 @@
 function three() {
-    let allSlides = document.querySelectorAll(".imgproduct_AP24");
-    for (let i = 0; i < allSlides.length; i++) {
-        if (allSlides[i].className.includes("hiddenSlide")) {
-            allSlides[i + 2].className = "imgproduct_AP24 activeSlide";
-            allSlides[i+1].className = "imgproduct_AP24 hiddenSlide";
+    let allImg = document.querySelectorAll(".imgproduct_AP24");
+    for (let i = 0; i < allImg.length; i++) {
+        if (allImg[i].className.includes("hiddenSlide")) {
+            allImg[i + 2].className = "imgproduct_AP24 activeSlide";
+            allImg[i+1].className = "imgproduct_AP24 hiddenSlide";
             break;
         } else {
-            allSlides[i + 2].className = "imgproduct_AP24 activeSlide";
-            allSlides[i].className = "imgproduct_AP24 hiddenSlide";
+            allImg[i + 2].className = "imgproduct_AP24 activeSlide";
+            allImg[i].className = "imgproduct_AP24 hiddenSlide";
             break;
         }
     }
 }
 function productimg_two() {
-    let allSlides = document.querySelectorAll(".imgproduct_AP24");
-    for (let i = 0; i < allSlides.length; i++) {
-        if (allSlides[i].className.includes("activeSlide")) {
-            allSlides[i + 1].className = "imgproduct_AP24 activeSlide";
-            allSlides[i].className = "imgproduct_AP24 hiddenSlide";
+    let allImg = document.querySelectorAll(".imgproduct_AP24");
+    for (let i = 0; i < allImg.length; i++) {
+        if (allImg[i].className.includes("activeSlide")) {
+            allImg[i + 1].className = "imgproduct_AP24 activeSlide";
+            allImg[i].className = "imgproduct_AP24 hiddenSlide";
 
             break;
         }
         else{
-            allSlides[i + 1].className = "imgproduct_AP24 activeSlide";
-            allSlides[i+2].className = "imgproduct_AP24 hiddenSlide";
+            allImg[i + 1].className = "imgproduct_AP24 activeSlide";
+            allImg[i+2].className = "imgproduct_AP24 hiddenSlide";
             break;
         }
     }
 }
 function productimg_once() {
-    let allSlides = document.querySelectorAll(".imgproduct_AP24");
-    for (let i = 0; i < allSlides.length; i++) {
-        if (allSlides[i].className.includes("hiddenSlide")) {
-            if (i + 1 == allSlides.length) {
-                allSlides[i].className = "imgproduct_AP24 activeSlide";
-                allSlides[i++].className = "imgproduct_AP24 hiddenSlide";
+    let allImg = document.querySelectorAll(".imgproduct_AP24");
+    for (let i = 0; i < allImg.length; i++) {
+        if (allImg[i].className.includes("hiddenSlide")) {
+            if (i + 1 == allImg.length) {
+                allImg[i].className = "imgproduct_AP24 activeSlide";
+                allImg[i++].className = "imgproduct_AP24 hiddenSlide";
                 break;
             }
             else{
-                allSlides[i+2].className = " imgproduct_AP24 hiddenSlide";
-                allSlides[i+1].className = "imgproduct_AP24 hiddenSlide";
-                allSlides[i].className = "imgproduct_AP24 activeSlide";
+                allImg[i+2].className = " imgproduct_AP24 hiddenSlide";
+                allImg[i+1].className = "imgproduct_AP24 hiddenSlide";
+                allImg[i].className = "imgproduct_AP24 activeSlide";
             }
         }
     }
